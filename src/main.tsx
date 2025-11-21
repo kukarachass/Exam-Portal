d10c4f9332bdcee4991d4d0095e1a608baee70f8
kukarachass <maksimvozniy18@gmail.com>
2025-11-21 17:04:12 +0100

    provider added

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {QueryClientProvider} from "@tanstack/react-query";
+import {RouterProvider} from "react-router-dom";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <QueryClientProvider client={}>
+        <RouterProvider router={}/>
+    </QueryClientProvider>
   </StrictMode>,
 )
