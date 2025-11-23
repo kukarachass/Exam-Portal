eae78763af005a637323fe6546cdb8d3d8f57747
kukarachass <maksimvozniy18@gmail.com>
2025-11-23 22:08:47 +0100

    mock endpoints added

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,19 +1,22 @@
 import {http, HttpResponse} from "msw";
 import {users} from "./data/users.ts";
-import type { ILoginRequest, IRegisterRequest } from "../interfaces/IAuth.ts";
+import type {ILoginRequest, IRegisterRequest} from "../interfaces/IAuth.ts";
 import type {IUser} from "../interfaces/IUser.ts";
+import type {IExam} from "../interfaces/IExam.ts";
+import {exams} from "./data/exams.ts";
+import {examAttempts, type IExamAttempt} from "./data/examAttempts.ts";
 
 export const handlers = [
-    http.post("/auth/login", async ({ request}) => {
-        const { email, password } = await request.json() as ILoginRequest;
+    http.post("/auth/login", async ({request}) => {
+        const {email, password} = await request.json() as ILoginRequest;
         const user = users.find(
             u => u.email === email && u.password === password
         );
 
         if (!user) {
             return HttpResponse.json(
-                { error: "Invalid email or password" },
-                { status: 401 }
+                {error: "Invalid email or password"},
+                {status: 401}
             )
         }
 
@@ -23,14 +26,14 @@ export const handlers = [
         })
     }),
 
-    http.post("/auth/register", async ({ request }) => {
-        const { email, password, name } = await request.json() as IRegisterRequest;
+    http.post("/auth/register", async ({request}) => {
+        const {email, password, name} = await request.json() as IRegisterRequest;
         const exists = users.find(u => u.email === email);
 
         if (exists) {
             return HttpResponse.json(
-                { error: "User already exists" },
-                { status: 400 }
+                {error: "User already exists"},
+                {status: 400}
             );
         }
 
@@ -50,13 +53,13 @@ export const handlers = [
         })
     }),
 
-    http.get("/auth/me", ({ request }) => {
+    http.get("/auth/me", ({request}) => {
         const token = request.headers.get("Authorization")?.replace("Bearer ", "");
 
         if (!token) {
             return HttpResponse.json(
-                { error: "Unauthorized" },
-                { status: 401 }
+                {error: "Unauthorized"},
+                {status: 401}
             );
         }
 
@@ -65,14 +68,58 @@ export const handlers = [
 
         if (!user) {
             return HttpResponse.json(
-                { error: "User not found" },
-                { status: 404 }
+                {error: "User not found"},
+                {status: 404}
             );
         }
 
         return HttpResponse.json(user);
     }),
 
+    http.post("exam/create", async ({request}) => {
+        const body = await request.json() as Omit<IExam, "id">;
+        const newExam = {
+            id: Date.now(),
+            ...body,
+        };
+
+        exams.push(newExam);
+
+        return HttpResponse.json(newExam, {status: 200})
+    }),
+
+    http.get("exam/all-exams", () => {
+        return HttpResponse.json(exams);
+    }),
+
+    http.get("/exams/:id", async ({params}) => {
+        const exam = exams.find(e => e.id === Number(params.id));
+        if (!exam) {
+            return HttpResponse.json({error: "Exam not found"}, {status: 404});
+        }
+
+        return HttpResponse.json(exam);
+    }),
+
+    http.post("exams/:id/grade", async ({params, request}) => {
+        const examId = Number(params.id);
+        const { studentId, grade, comment } = await request.json() as IExamAttempt;
+
+        const attempt = examAttempts.find(a => a.examId === examId && a.studentId === studentId);
+        if(!attempt) {
+            return HttpResponse.json({error: "Exam not found"}, {status: 404});
+        }
+
+        attempt.grade = grade;
+        attempt.comment = comment;
+        attempt.status = "graded";
+
+        return HttpResponse.json({
+            message: "graded",
+            attempt
+        })
+
+    })
 
 
 ]
